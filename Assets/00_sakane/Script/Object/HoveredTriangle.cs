2e84c8268723366d12681a4cba467512e8646bee
sakanekazuki0825 <sakane@qmax.co.jp>
2023-04-25 16:41:49 +0900

    選択中のUI表示

--- Assets/00_sakane/Script/Object/HoveredTriangle.cs
+++ Assets/00_sakane/Script/Object/HoveredTriangle.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class HoveredTriangle : MonoBehaviour
@@ -9,6 +7,6 @@ public class HoveredTriangle : MonoBehaviour
 
 	void Update()
 	{
-		
+		transform.Rotate(0, speed, 0);
 	}
 }
