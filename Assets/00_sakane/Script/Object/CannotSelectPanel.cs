bbdc9e87766c852db88bbdde1ea366dd7ecc8ef5
sakanekazuki0825 <sakane@qmax.co.jp>
2023-04-26 10:12:17 +0900

    選択出来ないパネルの挙動修正

--- Assets/00_sakane/Script/Object/CannotSelectPanel.cs
+++ Assets/00_sakane/Script/Object/CannotSelectPanel.cs
@@ -20,8 +20,15 @@ public class CannotSelectPanel : MonoBehaviour,ICannotSelectPanel
 
 		if (target != null)
 		{
-			var pos = target.transform.position + offset;
-			transform.position = new Vector3(pos.x, transform.position.y, pos.z);
+			//var pos = target.transform.position + offset;
+			//transform.position = new Vector3(pos.x, transform.position.y, pos.z);
+
+			var pos = target.transform.position - new Vector3(0.1f, 0, 0);
+			var number = pos.x / GameInstance.panelManager.SpriteSize.x;
+			number = Mathf.Floor(number);
+			pos = new Vector3(GameInstance.panelManager.SpriteSize.x * number - GameInstance.panelManager.SpriteSize.x / 2, 0, pos.z);
+
+			transform.position = new Vector3(pos.x - 0.01f,transform.position.y, pos.z);
 		}
 	}
 
